4627edd497347220b4c1514e793fde539dec600c
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-03-17 09:48:46 +0100

    done

--- user_statistics.py
+++ user_statistics.py
@@ -2,9 +2,9 @@
 from load_dataset import load_data
 
 
-def user_min_max(user_id):
+def user_min_max(user_id) -> str:
     '''
-    Parameters: None
+    Parameters: user_id
     Purpose: Reads the data and get the min and max transaction amount for a provided user
     Return: maximum and minimum transaction amount of the user
     '''
@@ -18,9 +18,9 @@ def user_min_max(user_id):
         return f'No user with id of {user_id} was found'
 
 
-def location_centroid(user_id):
+def location_centroid(user_id) -> list:
     '''
-    Parameters: None
+    Parameters: user_id
     Purpose: Reads the data and get the location centroid for a provided user
     Return: location centroid of the user
     '''
@@ -41,11 +41,11 @@ def location_centroid(user_id):
         return f'No user with id of {user_id} was found'
 
 
-def distance_from_centroid(user_id, transaction_id):
+def distance_from_centroid(user_id, transaction_id) -> float:
     '''
-    Parameters: None
-    Purpose: Reads the data and get the location centroid for a provided user
-    Return: location centroid of the user
+    Parameters: user_id and transaction_id
+    Purpose: finds the distance between a user transaction and the user location centroid for transactions
+    Return: distance between a user transaction and the user location centroid for transactions
     '''
     data = load_data()
     x_coordinates = []
@@ -74,7 +74,12 @@ def distance_from_centroid(user_id, transaction_id):
     except Exception:
         return f'An exception occurred, either due to wrong user id or transaction id does not match that particular user'
 
-def var_transaction_user(user_id):
+def var_transaction_user(user_id) -> float:
+    '''
+    Parameters: user_id
+    Purpose: calculates the variance of transaction for a provided user
+    Return: variance of transaction amount
+    '''
     data = load_data()
     list_amount = []
     try:
@@ -88,7 +93,7 @@ def var_transaction_user(user_id):
     except Exception:
         return f'No user with id of {user_id} was found'
 
-def std_transaction_user(user_id):
+def std_transaction_user(user_id) -> float:
     '''
     Parameters: user_id
     Purpose: calculates the standard deviatioin of transaction for a provided user
@@ -102,7 +107,7 @@ def std_transaction_user(user_id):
         return f'No user with id of {user_id} was found'
 
 
-def transaction_status(transaction_id):
+def transaction_status(transaction_id) -> dict:
     '''
     Parameters: transaction identity
     Purpose: determines whether a transaction is fraudulent or not and 
@@ -119,11 +124,11 @@ def transaction_status(transaction_id):
             else :
                 return f'No transaction with id of {transaction_id} was found'
 
-def distance_between_transaction_user(user_id):
+def distance_between_transaction_user(user_id) -> float:
     '''
     Parameters: user id
-    Purpose: takes the id of two different transactions and find the distance between them
-    Return: the distance between the two different transactions
+    Purpose: takes the id of a user, gets all the transaction id related to the user and find the distance between two transaction id selected 
+    Return: the distance between the two different transactions in particular to the user_id
     '''
     data = load_data()
     print('Choose the two respective transactions id from the list below')
@@ -148,7 +153,7 @@ def distance_between_transaction_user(user_id):
         return f'An exception occurred, either due to wrong user id or transaction id does not match that particular user'
 
 
-def distance_between_transaction_any_user(user_id1, user_id2):
+def distance_between_transaction_any_user(user_id1, user_id2) -> float:
     '''
     Parameters: user id
     Purpose: takes the id of two different transactions and find the distance between them
@@ -178,20 +183,4 @@ def distance_between_transaction_any_user(user_id1, user_id2):
     except Exception:
         return f'An exception occurred, either due to wrong user id or transaction id does not match that particular user'
 
-# min_max = user_min_max('21')
-# location_centroid = location_centroid('21')
-# distance_from_centroid1 = distance_from_centroid('21', '500004')
-# var_amount = var_transaction_user('21')
-# std_amount = std_transaction_user('21')
-# status = transaction_status('500002')
-# distance1 = distance_between_transaction_user('21')
-# distance2 = distance_between_transaction_any_user('21','22')
 
-# print(min_max)
-# print(location_centroid)
-# print(distance_from_centroid1)
-# print(var_amount)
-# print(std_amount)
-# print(status)
-# print(distance1)
-# print(distance2)

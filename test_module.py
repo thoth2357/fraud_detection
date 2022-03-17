1649bb0f3fea3d7a4630bd15b0329440fd135284
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-03-17 09:19:02 +0100

    main module interface implemented

---
+++ test_module.py
@@ -0,0 +1,74 @@
+import user_statistics
+def get_choice():
+    '''
+    Parameters: none 
+    Purpose: Asks user to enter a menu option, error checks so that only valid entries are accepted, returns valid entry.
+    Return: Valid choice -> str
+    '''
+    choice = None
+    while choice not in [str(numbers) for numbers in range(1,10)]:
+        choice = input('Enter a number: ')
+    return choice
+
+def main():
+    '''
+    Parameters: None
+    Purpose: provides a textual interface to interacts with user_statistics module.
+    Return: None
+    '''
+    print('\n\nFraud Detection')
+    print('Make a selection from the following choices:\n \
+            1. Maximum And Minimum Of User Transaction \n \
+            2. Find the location centroid of User transactions\n \
+            3. Find a User transaction location distance from the user location centroid  \n \
+            4. Find the Variance of User transactions amount \n \
+            5. Find the Standard Deviation of User transactions amount\n \
+            6. Find the Fraud status of a User transaction \n \
+            7. Find the distance between any two transaction location of a user \n \
+            8. Find the distance between any two transaction location of any two user \n \
+            9. Quit')
+        
+    choice = get_choice()
+    if choice == '9':
+        raise SystemExit
+    elif choice == '1':
+        user_id = input('Enter the user_id of the particular user you are concerned with ')
+        minimium_maximum_amount_user = user_statistics.user_min_max(user_id)
+        print(minimium_maximum_amount_user)
+    elif choice == '2':
+        user_id = input('Enter the user_id of the particular user you are concerned with ')
+        location_centroid_transaction_user = user_statistics.location_centroid(user_id)
+        print(location_centroid_transaction_user)
+    elif choice == '3':
+        user_id = input('Enter the user_id of the particular user you are concerned with ')
+        transaction_id = input('enter the user_id of the transaction you willing to find it distance from the centroid ')
+        transaction_distance_from_centroid = user_statistics.distance_from_centroid(user_id, transaction_id)
+        print(transaction_distance_from_centroid)
+    elif choice == '4':
+        user_id = input('Enter the user_id of the particular user you are concerned with ')
+        variance_transaction_amount_user = user_statistics.var_transaction_user(user_id)
+        print(variance_transaction_amount_user)
+    elif choice == '5':
+        user_id = input('Enter the user_id of the particular user you are concerned with ')
+        std_transaction_amount_user = user_statistics.std_transaction_user(user_id)
+        print(std_transaction_amount_user)
+    elif choice == '6':
+        user_id = input('Enter the transaction_id of the particular transaction you are concerned with checking it fraud status ')
+        fraud_status_transaction = user_statistics.transaction_status(user_id)
+        print(fraud_status_transaction)
+    elif choice == '7':
+        user_id = input('Enter the user_id of the particular user who among their transactions you are concerned with getting the distance between two transaction of the user ')
+        distance_transaction_user = user_statistics.distance_between_transaction_user(user_id)
+        print(distance_transaction_user)
+    elif choice =='8':
+        user_id = input('Enter the user_id of the particular user who among their transactions you are concerned with using one of their transaction ')
+        id2 = input('Enter the user_id of the particular user who among their transactions you are concerned with using one of their transaction ')
+        distance_transaction_any_user = user_statistics.distance_between_transaction_any_user(user_id, id2)
+        print(distance_transaction_any_user)
+
+
+try:
+    while True:
+        main()
+except SystemExit:
+    print('\nBYE FOR NOW!')
--- user_statistics.py
+++ user_statistics.py
@@ -10,9 +10,12 @@ def user_min_max(user_id):
     '''
     data = load_data()
     list_amount = []
-    for index in data[user_id]:
-        list_amount.append(index[2])
-    return f'User with id {user_id} has a maximum transaction amount of {max(list_amount)} and a minimum trasaction amount of {min(list_amount)}'
+    try:
+        for index in data[user_id]:
+            list_amount.append(index[2])
+        return f'User with id {user_id} has a maximum transaction amount of {max(list_amount)} and a minimum trasaction amount of {min(list_amount)}'
+    except Exception:
+        return f'No user with id of {user_id} was found'
 
 
 def location_centroid(user_id):
@@ -26,13 +29,17 @@ def location_centroid(user_id):
     x_coordinates = []
     y_coordinates = []
     summed_coordinates = []
-    for index in data[user_id]:
-        x_coordinates.append(float(index[3]))
-        y_coordinates.append(float(index[4]))
-    summed_coordinates.append(x_coordinates)
-    summed_coordinates.append(y_coordinates)
-    centroid = [sum(summed_coordinates[0])/len(summed_coordinates[0]),sum(summed_coordinates[1])/len(summed_coordinates[1])]
-    return centroid
+    try:
+        for index in data[user_id]:
+            x_coordinates.append(float(index[3]))
+            y_coordinates.append(float(index[4]))
+        summed_coordinates.append(x_coordinates)
+        summed_coordinates.append(y_coordinates)
+        centroid = [sum(summed_coordinates[0])/len(summed_coordinates[0]),sum(summed_coordinates[1])/len(summed_coordinates[1])]
+        return centroid
+    except Exception:
+        return f'No user with id of {user_id} was found'
+
 
 def distance_from_centroid(user_id, transaction_id):
     '''
@@ -44,35 +51,42 @@ def distance_from_centroid(user_id, transaction_id):
     x_coordinates = []
     y_coordinates = []
     summed_coordinates = []
-    for index in data[user_id]:
-        x_coordinates.append(float(index[3]))
-        y_coordinates.append(float(index[4]))
-    summed_coordinates.append(x_coordinates)
-    summed_coordinates.append(y_coordinates)
-    centroid = [sum(summed_coordinates[0])/len(summed_coordinates[0]),sum(summed_coordinates[1])/len(summed_coordinates[1])]
-    
-    transaction_location = []
-    for i in data.values():
-        for j in i:
-            if j[0] == str(transaction_id):
-                transaction_x_coord = float(j[3])
-                transaction_y_coord = float(j[4])
-                transaction_location.append(transaction_x_coord)
-                transaction_location.append(transaction_x_coord)
-    distance_from_centroid = ( (transaction_location[0] - centroid[0])**2 + (transaction_location[1] - centroid[1])**2 ) ** 0.5
-    
-    return distance_from_centroid
+    try:
+        for index in data[user_id]:
+            x_coordinates.append(float(index[3]))
+            y_coordinates.append(float(index[4]))
+        summed_coordinates.append(x_coordinates)
+        summed_coordinates.append(y_coordinates)
+        centroid = [sum(summed_coordinates[0])/len(summed_coordinates[0]),sum(summed_coordinates[1])/len(summed_coordinates[1])]
+    except Exception:
+        return f'No user with id of {user_id} was found'
+    try:
+        transaction_location = []
+        for i in data[user_id]:
+                if i[0] == str(transaction_id):
+                    transaction_x_coord = float(i[3])
+                    transaction_y_coord = float(i[4])
+                    transaction_location.append(transaction_x_coord)
+                    transaction_location.append(transaction_x_coord)
+        distance_from_centroid = ( (transaction_location[0] - centroid[0])**2 + (transaction_location[1] - centroid[1])**2 ) ** 0.5
+        
+        return distance_from_centroid
+    except Exception:
+        return f'An exception occurred, either due to wrong user id or transaction id does not match that particular user'
 
 def var_transaction_user(user_id):
     data = load_data()
     list_amount = []
-    for index in data[user_id]:
-        list_amount.append(float(index[2]))
-    n = len(list_amount)
-    mean = sum(list_amount) / n
-    deviations = [(x - mean) ** 2 for x in list_amount]
-    variance = sum(deviations) / n
-    return variance
+    try:
+        for index in data[user_id]:
+            list_amount.append(float(index[2]))
+        n = len(list_amount)
+        mean = sum(list_amount) / n
+        deviations = [(x - mean) ** 2 for x in list_amount]
+        variance = sum(deviations) / n
+        return variance
+    except Exception:
+        return f'No user with id of {user_id} was found'
 
 def std_transaction_user(user_id):
     '''
@@ -80,9 +94,13 @@ def std_transaction_user(user_id):
     Purpose: calculates the standard deviatioin of transaction for a provided user
     Return: standard deviation of transaction amount
     '''
-    var = var_transaction_user(user_id)
-    std_dev = var ** (1/2)
-    return std_dev
+    try:
+        var = var_transaction_user(user_id)
+        std_dev = var ** (1/2)
+        return std_dev
+    except Exception:
+        return f'No user with id of {user_id} was found'
+
 
 def transaction_status(transaction_id):
     '''
@@ -98,6 +116,8 @@ def transaction_status(transaction_id):
                     'status of the transaction':j[-1],
                     'description of transaction':j[1],
                 }
+            else :
+                return f'No transaction with id of {transaction_id} was found'
 
 def distance_between_transaction_user(user_id):
     '''
@@ -107,19 +127,25 @@ def distance_between_transaction_user(user_id):
     '''
     data = load_data()
     print('Choose the two respective transactions id from the list below')
-    for index in data[user_id]:
-        print(index[0], end=',')
+    try:
+        for index in data[user_id]:
+            print(index[0], end=',')
+    except Exception:
+        return f'No user with id of {user_id} was found'
+
     id_1 = input('\n enter the first transaction id from the list above ')
     id_2 = input('enter the second transaction id from the list above ')
     ids_coordinates = []
-    for ids in [id_1, id_2]:
-        for i in data.values():
-            for j in i:
-                if j[0] == str(ids):
-                    id_coordinates = [float(j[3]), float(j[4])]
-                    ids_coordinates.append(id_coordinates)
-    distance = ((ids_coordinates[0][0] - ids_coordinates[1][0])**2 + (ids_coordinates[0][1] - ids_coordinates[1][1])**2)**0.5
-    return distance
+    try:
+        for ids in [id_1, id_2]:
+            for i in data[user_id]:
+                    if i[0] == str(ids):
+                        id_coordinates = [float(i[3]), float(i[4])]
+                        ids_coordinates.append(id_coordinates)
+        distance = ((ids_coordinates[0][0] - ids_coordinates[1][0])**2 + (ids_coordinates[0][1] - ids_coordinates[1][1])**2)**0.5
+        return distance
+    except Exception:
+        return f'An exception occurred, either due to wrong user id or transaction id does not match that particular user'
 
 
 def distance_between_transaction_any_user(user_id1, user_id2):
@@ -146,20 +172,20 @@ def distance_between_transaction_any_user(user_id1, user_id2):
     distance = ((ids_coordinates[0][0] - ids_coordinates[1][0])**2 + (ids_coordinates[0][1] - ids_coordinates[1][1])**2)**0.5
     return distance
 
-min_max = user_min_max('21')
-location_centroid = location_centroid('21')
-distance_from_centroid1 = distance_from_centroid('21', '500004')
-var_amount = var_transaction_user('21')
-std_amount = std_transaction_user('21')
-status = transaction_status('500002')
-distance1 = distance_between_transaction_user('21')
-distance2 = distance_between_transaction_any_user('21','22')
+# min_max = user_min_max('21')
+# location_centroid = location_centroid('21')
+# distance_from_centroid1 = distance_from_centroid('21', '500004')
+# var_amount = var_transaction_user('21')
+# std_amount = std_transaction_user('21')
+# status = transaction_status('500002')
+# distance1 = distance_between_transaction_user('21')
+# distance2 = distance_between_transaction_any_user('21','22')
 
-print(min_max)
-print(location_centroid)
-print(distance_from_centroid1)
-print(var_amount)
-print(std_amount)
-print(status)
-print(distance1)
-print(distance2)
+# print(min_max)
+# print(location_centroid)
+# print(distance_from_centroid1)
+# print(var_amount)
+# print(std_amount)
+# print(status)
+# print(distance1)
+# print(distance2)
